03892a4402965777fe896e18839d68f6af492c05
bigsmall2697 <bigsmall2697@gmail.com>
2024-12-18 13:23:40 -0500

    fix: app config for deployment

--- app.js
+++ app.js
@@ -2,25 +2,31 @@ const config                = require('./config/index.config.js');
 const Cortex                = require('ion-cortex');
 const ManagersLoader        = require('./loaders/ManagersLoader.js');
 
-const mongoDB = config.dotEnv.MONGO_URI? require('./connect/mongo')({
-    uri: config.dotEnv.MONGO_URI
-}):null;
+// const mongoDB = config.dotEnv.MONGO_URI? require('./connect/mongo')({
+//     uri: config.dotEnv.MONGO_URI
+// }):null;
 
-const cache = require('./cache/cache.dbh')({
-    prefix: config.dotEnv.CACHE_PREFIX ,
-    url: config.dotEnv.CACHE_REDIS
-});
+// const cache = require('./cache/cache.dbh')({
+//     prefix: config.dotEnv.CACHE_PREFIX ,
+//     url: config.dotEnv.CACHE_REDIS
+// });
+const cache = {}
 
-const cortex = new Cortex({
-    prefix: config.dotEnv.CORTEX_PREFIX,
-    url: config.dotEnv.CORTEX_REDIS,
-    type: config.dotEnv.CORTEX_TYPE,
-    state: ()=>{
-        return {} 
-    },
-    activeDelay: "50ms",
-    idlDelay: "200ms",
-});
+// const cortex = new Cortex({
+//     prefix: config.dotEnv.CORTEX_PREFIX,
+//     url: config.dotEnv.CORTEX_REDIS,
+//     type: config.dotEnv.CORTEX_TYPE,
+//     state: ()=>{
+//         return {} 
+//     },
+//     activeDelay: "50ms",
+//     idlDelay: "200ms",
+// });
+const cortex = {
+    nodeType: '',
+    sub: () => {},
+    AsyncEmitToAllOf: () => {}
+}
 
 
 
